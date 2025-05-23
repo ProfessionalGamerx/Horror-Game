e7a669484a822ceeb1ba95f36484de701a3e633a
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-23 14:41:25 +1200

    added interactable door script

--- hinge.gd
+++ hinge.gd
@@ -1,19 +1,11 @@
-extends Node3D  # Or Area3D if using it for trigger-based interaction
+extends Node3D
 
-@export var interaction_distance = 3.0
-@export var player_path : NodePath  # Set this to the player in the inspector
-
-var is_open = false
+@onready var animation_player: AnimationPlayer = $DoorAnimation
+var isdooropen = false
 
 func _process(delta):
-	if Input.is_action_just_pressed("interact"):  # You will set this to "E"
-		var player = get_node(player_path)
-		if player and global_position.distance_to(player.global_position) <= interaction_distance:
-			toggle_door()
-
-func toggle_door():
-	if is_open:
-		$AnimationPlayer.play_backwards("open")
-	else:
-		$AnimationPlayer.play("open")
-	is_open = !is_open
+	if Input.is_action_just_pressed("interact"):
+		if not animation_player.is_playing ():
+			animation_player.play("door_open")
+			
+	elif 
