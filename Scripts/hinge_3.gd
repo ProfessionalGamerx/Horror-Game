1d188b93c4084fd0038f0c2fe69e89307c98e56d
ProfessionalGamerx <24684@burnside.school.nz>
2025-06-03 09:36:44 +1200

    finished interactable doors and places them in doorways

--- Scripts/hinge_3.gd
+++ Scripts/hinge_3.gd
@@ -1,15 +1,15 @@
 extends Node3D
 
-@onready var anim = $"DoorAnimations"
-@onready var is_open = false
+@onready var anim = $"DoorAnimations3"
+@onready var is_open = true
 
 func interact():
 	print("Interacted with", self.name)
 	if is_open:
-		anim.play("door_close")
+		anim.play("door_close3")
 		await get_tree().create_timer(0.5).timeout
 		is_open = false
 	else:
-		anim.play("door_open")
+		anim.play("door_open3")
 		await get_tree().create_timer(0.5).timeout
 		is_open = true
